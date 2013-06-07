2ddc5fb8403a7ff3e631f56beb9ee283c851e178
zdrav4enceto@gmail.com <zdravka.sandova@novarto.com>
2013-06-07 12:03:13 +0300

    Bonus scores

---
+++ BonusScores/BonusScores.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+namespace BonusScores
+{
+    class BonusScores
+    {
+        static void Main()
+        {
+            Console.Write("Enter number: ");
+            int number;
+            int.TryParse(Console.ReadLine(), out number);
+
+            switch (Enumerable.Range(1, 9).Contains(number))
+            {
+                case true:
+                    if (Enumerable.Range(1, 9).Contains(number))
+                    {
+                        if (Enumerable.Range(1, 3).Contains(number))
+                        {
+                            switch (number)
+                            {
+                                case 1:
+                                case 2:
+                                case 3:
+                                    Console.WriteLine("result: {0} * 10 = {1}", number, number*10);
+                                    break;
+                            }
+                        }
+                        else if (Enumerable.Range(4, 6).Contains(number))
+                        {
+                            switch (number)
+                            {
+                                case 4:
+                                case 5:
+                                case 6:
+                                    Console.WriteLine("result: {0} * 100 = {1}", number, number*100);
+                                    break;
+                            }
+                        }
+                        else if (Enumerable.Range(7, 9).Contains(number))
+                        {
+                            switch (number)
+                            {
+                                case 7:
+                                case 8:
+                                case 9:
+                                    Console.WriteLine("result: {0} * 1000 = {1}", number, number*1000);
+                                    break;
+                            }
+                        }
+
+                    }
+
+                    break;
+
+               default: Console.WriteLine("ERR!");
+
+                    break;
+            }
+        }
+    }
+}
